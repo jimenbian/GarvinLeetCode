ec276a0f25aa1aeed11205a2dbfcddb09c5a4cf6
jimenbian <jimenbian@gmail.com>
2015-02-13 16:51:27 +0800

    add no 36

---
+++ src/No36/ValidSudoku/Solution.java
@@ -0,0 +1,16 @@
+package No36.ValidSudoku;
+
+public class Solution {
+    
+//  public static boolean isValidSudoku(char[][] board) {
+//	  char[][] board1={{'2','3','4'},{'3','4','5'}};
+//      char[] board2={'3','4','5'};
+//      String a="fa";
+//      board1.length
+//    }
+    public static void main(String[] args){
+    	  char[][] board1={{2,4,6},{'3','4','5'}};
+    	  System.out.print(""+(int)board1[0][0]);
+    	//   Character.isDigit(board1[0][0]);
+    }
+}
